8fdb221ea7b5675140a380963ddaa06c8bd40171
IT24104013 <it24104013@my.sliit.lk>
2025-09-16 22:40:44 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24104013.R
@@ -0,0 +1,23 @@
+setwd("C:\\Users\\User\\Desktop\\IT24104013")
+
+#1)
+# uniform between 0 and 40
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+#2)
+# rate = 1/3
+pexp(2, rate = 1/3, lower.tail = TRUE) 
+
+#3)
+#i)
+pnorm(130, mean=100, sd=15,lower.tail=FALSE)
+
+
+#ii)
+qnorm(0.95, mean=100, sd=15)
+
+
+
+  
+
+
